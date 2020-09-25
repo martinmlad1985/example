870b24660dfa2e52652456576449654610bc6c3e
martinmlad1985 <60547325+martinmlad1985@users.noreply.github.com>
2020-09-25 21:41:11 +0300

    Updates

---
+++ precache-manifest.d25fd9e36e3790f8e6ca9f88de85da1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b9edb3955f6b93713f538d820cf4e",
+    "url": "/example/index.html"
+  },
+  {
+    "revision": "4405db83e419a524cb2c",
+    "url": "/example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6711b79880b62ebeabea",
+    "url": "/example/static/js/2.f5498493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/example/static/js/2.f5498493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4405db83e419a524cb2c",
+    "url": "/example/static/js/main.e2d9d643.chunk.js"
+  },
+  {
+    "revision": "20a112f0d399229eebba",
+    "url": "/example/static/js/runtime-main.3d67e3ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example/static/media/logo.5d5d9eef.svg"
+  }
+]);
